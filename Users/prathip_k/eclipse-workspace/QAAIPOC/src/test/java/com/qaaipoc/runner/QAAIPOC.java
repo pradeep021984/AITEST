91069b0ce4d95435f1331e7e3e5a737e4bc9336c
QAIPOC <73299053+pradeep021984@users.noreply.github.com>
2024-11-22 17:26:20 +0530

    Updating the code
    Updating the code

--- Users/prathip_k/eclipse-workspace/QAAIPOC/src/test/java/com/qaaipoc/runner/QAAIPOC.java
+++ Users/prathip_k/eclipse-workspace/QAAIPOC/src/test/java/com/qaaipoc/runner/QAAIPOC.java
@@ -1,7 +1,275 @@
-package com.qaaipoc;
+package com.qaaipoc.runner;
 
-public class QAAIPOC {
-    public static void main(String[] args) {
-        System.out.println("Hello, This is sample Test Run");
+import org.testng.annotations.Test;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import org.apache.http.entity.StringEntity;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qaaipoc.utlities.ExcelReader;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.io.BufferedWriter;
+import java.io.Writer;
+
+
+import okhttp3.*;
+import java.io.IOException;
+import java.util.Base64;
+
+public class QAAIPOC extends ExcelReader {
+
+    private static String JIRA_URL, USER_EMAIL, JIRA_API_TOKEN, OPENAI_URL, OPENAI_API_TOKEN, OPENAI_TC_PROMPT,
+            OPENAI_AI_CODE_PROMPT, JIRA_ID;
+    static ExcelReader reader = new ExcelReader();
+
+    private static final String GITHUB_API_URL = "https://api.github.com";
+    private static final String REPO_OWNER = "pradeep021984";  // Replace with the repo owner
+    private static final String REPO_NAME = "AITEST";  // Replace with the repository name
+    private static final String ACCESS_TOKEN = "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";  // Replace with your GitHub token
+
+    @Test
+    private void runAIPOC() throws Exception {
+
+        System.out.println("Sample Test");
+//		String filePath = "Users/prathip_k/eclipse-workspace/QAAIPOC/src/test/java/com/qaaipoc/runner/QAAIPOC.java";  // Replace with the file path
+//	       String fileContent = "Hello, GitHub!";     // Replace with the content of the file
+//	       String commitMessage = "Upload file via API";
+//
+//	       uploadFileToGitHub(filePath, fileContent, commitMessage);
+
+
+        getDataFromExcel();
+        String userStory = getJIRAValues(JIRA_ID);
+        if (!userStory.contains("ERROR")) {
+            System.out.println("Writing feature file for  '" + JIRA_ID + "' - " + strbasePath + "/src/test/resources/feature/");
+            String testCases = openAI(OPENAI_TC_PROMPT + " \"" + userStory + "\"");
+
+            testCases= testCases.replaceAll("gherkin", "").replaceAll("#", "").replace("`", "");
+            writeToFile(strbasePath + "/src/test/resources/feature/", JIRA_ID + ".feature", testCases);
+            uploadFileToGitHub(strbasePath.substring(1) +"/src/test/resources/feature/" + JIRA_ID + ".feature",testCases,"Uploaded from AI Script");
+
+            System.out.println("Generating Automation Code");
+            String txtCode = openAI(OPENAI_AI_CODE_PROMPT + " \"" + testCases + "\"").replace("```gherkin", "")
+                    .replace("```java", "");
+
+            createFiles(txtCode);
+            System.out.println("Execution is Completed! Pls check output folder");
+        }
+
+
     }
+
+    public static String getJIRAValues(String issueKey) throws ClientProtocolException, IOException {
+        StringBuilder userStory = new StringBuilder();
+        try {
+            String issueSummary, issueDescription, textOutput = "";
+
+            System.out.println("Reading '" + JIRA_ID + "' User Story from JIRA");
+
+            String auth = USER_EMAIL + ":" + JIRA_API_TOKEN;
+            byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.ISO_8859_1));
+            String authHeader = "Basic " + new String(encodedAuth);
+
+            CloseableHttpClient client = HttpClients.createDefault();
+
+            HttpGet httpGet = new HttpGet(JIRA_URL + issueKey);
+            httpGet.setHeader("Authorization", authHeader);
+            httpGet.setHeader("Accept", "application/json");
+
+            HttpResponse response = client.execute(httpGet);
+            HttpEntity responseEntity = response.getEntity();
+            String result = EntityUtils.toString(responseEntity);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(result);
+            issueSummary = jsonNode.get("fields").get("summary").asText();
+            issueDescription = jsonNode.get("fields").get("description").toPrettyString();
+            client.close();
+
+            JSONObject jsonObjectContent = new JSONObject(issueDescription);
+            JSONArray arrContent = jsonObjectContent.getJSONArray("content");
+            for (int intContent = 0; intContent < arrContent.length(); intContent++) {
+                textOutput = "";
+                JSONObject jsonObjectSubContent = arrContent.getJSONObject(intContent);
+                JSONArray arrSubContent = jsonObjectSubContent.getJSONArray("content");
+                for (int intSubContent = 0; intSubContent < arrSubContent.length(); intSubContent++) {
+                    JSONObject jsonobjectText = arrSubContent.getJSONObject(intSubContent);
+                    if (jsonobjectText.has("text")) {
+                        textOutput = textOutput + " " + jsonobjectText.getString("text");
+                    }
+
+                }
+                userStory.append(textOutput.trim());
+            }
+
+            return (userStory.toString());
+
+        } catch (Exception e) {
+            userStory.append("ERROR");
+            userStory.append(e.getMessage());
+            return (userStory.toString());
+        }
+    }
+
+    public static String openAI(String text) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost(OPENAI_URL);
+        httpPost.setHeader("Content-Type", "application/json");
+
+        Map<String, Object> contents = new HashMap<>();
+        Map<String, Object> parts = new HashMap<>();
+        Map<String, Object> part = new HashMap<>();
+
+        part.put("text", text);
+        parts.put("parts", new Object[] { part });
+        contents.put("contents", new Object[] { parts });
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequest = objectMapper.writeValueAsString(contents);
+
+        HttpEntity stringEntity = new StringEntity(jsonRequest);
+        httpPost.setEntity(stringEntity);
+
+        HttpResponse response = httpClient.execute(httpPost);
+        HttpEntity responseEntity = response.getEntity();
+
+        String responseString = EntityUtils.toString(responseEntity);
+
+        JsonNode jsonResponse = objectMapper.readTree(responseString);
+
+        String assistantReply = jsonResponse.get("candidates").get(0).get("content").get("parts").get(0).get("text")
+                .asText();
+
+        httpClient.close();
+        return assistantReply;
+    }
+
+    private static void getDataFromExcel() throws InvalidFormatException, IOException {
+        List<Map<String, String>> data = reader.getData(SheetPath, "POC");
+        JIRA_URL = data.get(0).get("JiraUrl");
+        USER_EMAIL = data.get(0).get("UserEmail");
+        JIRA_API_TOKEN = data.get(0).get("JiraAPIToken");
+        OPENAI_API_TOKEN = data.get(0).get("OpenAIAPIToken");
+        OPENAI_URL = data.get(0).get("OpenAIUrl") + OPENAI_API_TOKEN;
+        OPENAI_TC_PROMPT = data.get(0).get("OpenAITCPrompt");
+        OPENAI_AI_CODE_PROMPT = data.get(0).get("OpenAITCodePrompt");
+        JIRA_ID = data.get(0).get("JiraID");
+    }
+
+    private static void writeToFile(String folderPath, String fileName, String testToWrite) {
+        try {
+            File filePath = new File(folderPath + fileName);
+            boolean result = Files.deleteIfExists(filePath.toPath());
+            FileOutputStream opStream = new FileOutputStream(filePath);
+            OutputStreamWriter opsWriter = new OutputStreamWriter(opStream);
+            Writer bufWritter = new BufferedWriter(opsWriter);
+            bufWritter.write(testToWrite);
+            bufWritter.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the file statsTest.txt");
+        }
+    }
+
+    public static void createFiles(String txtOutput) throws IOException {
+        String pomFile, featureFile, stepFile,firstLine;
+        String[] arrOutput = txtOutput.split("class ");
+        stepFile=arrOutput[0] + " class " + arrOutput[1];
+        pomFile="class " + arrOutput[2];
+
+
+        System.out.println("Writing Step Definition for  '" + JIRA_ID + "' - " + strbasePath + "/src/test/java/com/qaaipoc/stepDefinition/");
+        writeToFile(strbasePath +"/src/test/java/com/qaaipoc/stepDefinition/", JIRA_ID + "_StepDefinition.java",
+                stepFile);
+        uploadFileToGitHub(strbasePath.substring(1) +"/src/test/java/com/qaaipoc/stepDefinition/" + JIRA_ID + "_StepDefinition.java",stepFile,"Uploaded from AI Script");
+
+        System.out.println("Writing POM file for  '" + JIRA_ID + "' - " + strbasePath + "/src/test/java/com/qaaipoc/pageObject/");
+        writeToFile(strbasePath +"/src/test/java/com/qaaipoc/pageObject/", JIRA_ID + "_Page.java", pomFile);
+        uploadFileToGitHub(strbasePath.substring(1) +"/src/test/java/com/qaaipoc/pageObject/" + JIRA_ID + "_Page.java",pomFile,"Uploaded from AI Script");
+    }
+
+
+    public static void createFiles_old(String txtOutput) {
+        String pomFile, featureFile, stepFile,firstLine;
+        featureFile = txtOutput.split("Feature File")[1].split("`")[0].toString();
+        String featureName = txtOutput.substring(0, txtOutput.indexOf("\n")).replace(":", "").replace(" ", "")
+                .replace("##", "");
+        firstLine = featureFile.substring(0, featureFile.indexOf("\n"));
+        featureFile=featureFile.replace(firstLine, "");
+
+        stepFile = txtOutput.split("Step Definition")[1].split("`")[0].toString();
+        firstLine = stepFile.substring(0, stepFile.indexOf("\n"));
+        stepFile=stepFile.replace(firstLine, "");
+
+
+        if (txtOutput.contains("POM")) {
+            pomFile = txtOutput.split("POM")[1].split("`")[0].toString();
+            firstLine = pomFile.substring(0, pomFile.indexOf("\n"));
+            pomFile=pomFile.replace(firstLine, "");
+        } else {
+            pomFile = txtOutput.split("Page Object Model")[1].split("`")[0].toString();
+            firstLine = pomFile.substring(0, pomFile.indexOf("\n"));
+            pomFile=pomFile.replace(firstLine, "");
+        }
+
+
+
+        System.out.println("Writing Step Definition for  '" + JIRA_ID + "' - " + strbasePath + "/src/test/resources/feature/");
+        writeToFile(strbasePath +"/src/test/java/com/qaaipoc/stepDefinition/", JIRA_ID + "_StepDefinition.java",
+                stepFile);
+
+        System.out.println("Writing POM file for  '" + JIRA_ID + "' - " + strbasePath + "/src/test/resources/feature/");
+        writeToFile(strbasePath +"/src/test/java/com/qaaipoc/pageObject/", JIRA_ID + "_Page.java", pomFile);
+    }
+
+
+    public static void uploadFileToGitHub(String filePath, String content, String commitMessage) throws IOException {
+        String base64Content = Base64.getEncoder().encodeToString(content.getBytes());
+        String jsonPayload = String.format(
+                "{ \"message\": \"%s\", \"content\": \"%s\" }",
+                commitMessage, base64Content
+        );
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(String.format("%s/repos/%s/%s/contents/%s", GITHUB_API_URL, REPO_OWNER, REPO_NAME, filePath))
+                .header("Authorization", "Bearer " + ACCESS_TOKEN)
+                .header("Accept", "application/vnd.github.v3+json")
+                .put(RequestBody.create(jsonPayload, MediaType.parse("application/json")))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("File uploaded successfully!");
+                System.out.println("Response: " + response.body().string());
+            } else {
+                System.err.println("Failed to upload file. HTTP Code: " + response.code());
+                System.err.println("Error: " + response.body().string());
+            }
+        }
+
+    }
+
 }
+
--- Users/prathip_k/eclipse-workspace/QAAIPOC/src/test/java/com/qaaipoc/utlities/ExcelReader.java
+++ Users/prathip_k/eclipse-workspace/QAAIPOC/src/test/java/com/qaaipoc/utlities/ExcelReader.java
@@ -1,1 +1,312 @@
-// Hello, GitHub!
+
+package com.qaaipoc.utlities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ExcelReader {
+    public static String SheetPath = "/Users/prathip_k/Documents/OpenAIPOC.xlsx",SheetPathValue="Local";
+    public static String strbasePath = new File("").getAbsolutePath();
+    String reportSheetPath = SheetPath;
+    public List<Map<String, String>> getData(String excelFilePath, String sheetName)
+            throws InvalidFormatException, IOException {
+        Sheet sheet = getSheetByName(excelFilePath, sheetName);
+        return readSheet(sheet);
+
+    }
+
+    public List<Map<String, String>> getData(String excelFilePath, int sheetNumber)
+            throws InvalidFormatException, IOException {
+        Sheet sheet = getSheetByIndex(excelFilePath, sheetNumber);
+        return readSheet(sheet);
+    }
+
+    private Sheet getSheetByName(String excelFilePath, String sheetName) throws IOException, InvalidFormatException {
+        Sheet sheet = null;
+        sheet = getWorkBook(excelFilePath).getSheet(sheetName);
+        return sheet;
+    }
+
+    private Sheet getSheetByIndex(String excelFilePath, int sheetNumber) throws IOException, InvalidFormatException {
+        Sheet sheet = getWorkBook(excelFilePath).getSheetAt(sheetNumber);
+        return sheet;
+    }
+
+    public Workbook getWorkBook(String excelFilePath) throws IOException, InvalidFormatException {
+        try {
+            return WorkbookFactory.create(new File(excelFilePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        //	return WorkbookFactory.create(new File(excelFilePath));
+    }
+
+    private List<Map<String, String>> readSheet(Sheet sheet) {
+        Row row;
+        int totalRow = sheet.getPhysicalNumberOfRows();
+        List<Map<String, String>> excelRows = new ArrayList<Map<String, String>>();
+        int headerRowNumber = getHeaderRowNumber(sheet);
+        if (headerRowNumber != -1) {
+            int totalColumn = sheet.getRow(headerRowNumber).getLastCellNum();
+            int setCurrentRow = 1;
+            for (int currentRow = setCurrentRow; currentRow <= totalRow; currentRow++) {
+                row = getRow(sheet, sheet.getFirstRowNum() + currentRow);
+                LinkedHashMap<String, String> columnMapdata = new LinkedHashMap<String, String>();
+                for (int currentColumn = 0; currentColumn < totalColumn; currentColumn++) {
+                    columnMapdata.putAll(getCellValue(sheet, row, currentColumn));
+                }
+                excelRows.add(columnMapdata);
+            }
+        }
+        return excelRows;
+    }
+
+    private int getHeaderRowNumber(Sheet sheet) {
+        Row row;
+        int totalRow = sheet.getLastRowNum();
+        for (int currentRow = 0; currentRow <= totalRow + 1; currentRow++) {
+            row = getRow(sheet, currentRow);
+            if (row != null) {
+                int totalColumn = row.getLastCellNum();
+                for (int currentColumn = 0; currentColumn < totalColumn; currentColumn++) {
+                    Cell cell;
+                    cell = row.getCell(currentColumn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                    if (cell.getCellType() == CellType.STRING) {
+                        return row.getRowNum();
+
+
+                    } else if (cell.getCellType() == CellType.NUMERIC) {
+                        return row.getRowNum();
+
+                    } else if (cell.getCellType() == CellType.BOOLEAN) {
+                        return row.getRowNum();
+                    } else if (cell.getCellType() == CellType.ERROR) {
+                        return row.getRowNum();
+                    }
+                }
+            }
+        }
+        return (-1);
+    }
+
+    private Row getRow(Sheet sheet, int rowNumber) {
+        return sheet.getRow(rowNumber);
+    }
+
+    private LinkedHashMap<String, String> getCellValue(Sheet sheet, Row row, int currentColumn) {
+        LinkedHashMap<String, String> columnMapdata = new LinkedHashMap<String, String>();
+        Cell cell;
+        if (row == null) {
+            if (sheet.getRow(sheet.getFirstRowNum()).getCell(currentColumn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                    .getCellType() != CellType.BLANK) {
+                String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(currentColumn)
+                        .getStringCellValue();
+                columnMapdata.put(columnHeaderName, "");
+            }
+        } else {
+            cell = row.getCell(currentColumn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+            if (cell.getCellType() == CellType.STRING) {
+                if (sheet.getRow(sheet.getFirstRowNum())
+                        .getCell(cell.getColumnIndex(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        .getCellType() != CellType.BLANK) {
+                    String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+                            .getStringCellValue();
+                    columnMapdata.put(columnHeaderName, cell.getStringCellValue());
+                }
+            } else if (cell.getCellType() == CellType.NUMERIC) {
+                if (sheet.getRow(sheet.getFirstRowNum())
+                        .getCell(cell.getColumnIndex(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        .getCellType() != CellType.BLANK) {
+                    String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+                            .getStringCellValue();
+                    columnMapdata.put(columnHeaderName, NumberToTextConverter.toText(cell.getNumericCellValue()));
+                }
+            } else if (cell.getCellType() == CellType.BLANK) {
+                if (sheet.getRow(sheet.getFirstRowNum())
+                        .getCell(cell.getColumnIndex(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        .getCellType() != CellType.BLANK) {
+                    String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+                            .getStringCellValue();
+                    columnMapdata.put(columnHeaderName, "");
+                }
+            } else if (cell.getCellType() == CellType.BOOLEAN) {
+                if (sheet.getRow(sheet.getFirstRowNum())
+                        .getCell(cell.getColumnIndex(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        .getCellType() != CellType.BLANK) {
+                    String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+                            .getStringCellValue();
+                    columnMapdata.put(columnHeaderName, Boolean.toString(cell.getBooleanCellValue()));
+                }
+            } else if (cell.getCellType() == CellType.ERROR) {
+                if (sheet.getRow(sheet.getFirstRowNum())
+                        .getCell(cell.getColumnIndex(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        .getCellType() != CellType.BLANK) {
+                    String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+                            .getStringCellValue();
+                    columnMapdata.put(columnHeaderName, Byte.toString(cell.getErrorCellValue()));
+                }
+            }
+        }
+        return columnMapdata;
+    }
+
+
+
+    public FileInputStream fis = null;
+    public FileOutputStream fileOut = null;
+    private XSSFWorkbook workbook = null;
+    private XSSFSheet sheet = null;
+    private XSSFRow row = null;
+    private XSSFCell cell = null;
+
+    public boolean setCellData(String sheetName, String colName, int rowNum, String data) {
+        try {
+            fis = new FileInputStream(SheetPath);
+            workbook = new XSSFWorkbook(fis);
+
+            int index = workbook.getSheetIndex(sheetName);
+            int colNum = -1;
+            if (index == -1)
+                return false;
+
+            sheet = workbook.getSheetAt(index);
+
+            row = sheet.getRow(0);
+            for (int i = 0; i < row.getLastCellNum(); i++) {
+//			 System.out.println(row.getCell(i).getStringCellValue().trim());
+                if (row.getCell(i).getStringCellValue().trim().equals(colName))
+                    colNum = i;
+            }
+            if (colNum == -1)
+                return false;
+
+            sheet.autoSizeColumn(colNum);
+            row = sheet.getRow(rowNum - 1);
+            if (row == null)
+                row = sheet.createRow(rowNum - 1);
+
+            cell = row.getCell(colNum);
+            if (cell == null)
+                cell = row.createCell(colNum);
+
+            cell.setCellValue(data);
+
+            fileOut = new FileOutputStream(SheetPath);
+
+            workbook.write(fileOut);
+
+            fileOut.close();
+            workbook.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public boolean setReportCellData(String sheetName, String colName, int rowNum, String data) {
+        try {
+            fis = new FileInputStream(reportSheetPath);
+            workbook = new XSSFWorkbook(fis);
+
+            int index = workbook.getSheetIndex(sheetName);
+            int colNum = -1;
+            if (index == -1)
+                return false;
+
+            sheet = workbook.getSheetAt(index);
+            row = sheet.getRow(0);
+            for (int i = 0; i < row.getLastCellNum(); i++) {
+//    			 System.out.println(row.getCell(i).getStringCellValue().trim());
+                if (row.getCell(i).getStringCellValue().trim().equals(colName))
+                    colNum = i;
+            }
+            if (colNum == -1)
+                return false;
+
+            sheet.autoSizeColumn(colNum);
+            row = sheet.getRow(rowNum);
+            if (row == null)
+                row = sheet.createRow(rowNum);
+
+            cell = row.getCell(colNum);
+            if (cell == null)
+                cell = row.createCell(colNum);
+
+            cell.setCellValue(data);
+
+            fileOut = new FileOutputStream(reportSheetPath);
+
+            workbook.write(fileOut);
+
+            fileOut.close();
+            workbook.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void insertValue() throws ClassNotFoundException {
+        // Database credentials
+
+
+        // Establish a database connection
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con=DriverManager.getConnection("jdbc:ucanaccess:/"+reportSheetPath);
+            Statement st = con.createStatement();
+            // SQL insert query
+            String insertSQL = "INSERT INTO "+reportSheetPath+"$ VALUES ('SCENARIO','Log','PASS')";
+            st.executeQuery( insertSQL);
+
+        } catch (SQLException e) {
+            // Handle any SQL exceptions
+            e.printStackTrace();
+        }
+
+    }
+
+}
